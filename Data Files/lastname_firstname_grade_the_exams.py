0454ab08b0902a36af379a376c7c6d0ff626c724
Luan Nguyen <nguyenngocluan.298@gmail.com>
2022-03-11 00:39:49 +0700

    First commit

--- Data Files/lastname_firstname_grade_the_exams.py
+++ Data Files/lastname_firstname_grade_the_exams.py
@@ -1,1 +1,7 @@
 filename = input("Enter a filename: ")
+try:
+    with open(filename, "r") as file1:
+        FileContent = file1.read()
+        print(FileContent)
+except:
+    print("File cannot found!")
