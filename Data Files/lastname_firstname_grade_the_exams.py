db9aeb90f3f2e771366025fe7bec12a9148d7d0b
Luan Nguyen <nguyenngocluan.298@gmail.com>
2022-03-11 00:39:49 +0700

    First commit

--- Data Files/lastname_firstname_grade_the_exams.py
+++ Data Files/lastname_firstname_grade_the_exams.py
@@ -1,7 +1,13 @@
+import numpy as np
+import pandas as pd
+
 filename = input("Enter a filename: ")
 try:
     with open(filename, "r") as file1:
-        FileContent = file1.read()
-        print(FileContent)
+        answer_list = [line.split() for line in file1]
+    print(f"Successfully opened {filename}")
 except:
-    print("File cannot found!")
+    print("File cannot be found!")
+
+np_answer = np.array(answer_list)
+print(np_answer)
