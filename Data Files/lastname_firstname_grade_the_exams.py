0cc88ccdcc9ca448bdd95353918db3e8bbcb1a95
Luan Nguyen <nguyenngocluan.298@gmail.com>
2022-03-13 23:53:13 +0700

    Finish task 2.1

--- Data Files/lastname_firstname_grade_the_exams.py
+++ Data Files/lastname_firstname_grade_the_exams.py
@@ -1,12 +1,63 @@
+import re
+
 import numpy as np
 import pandas as pd
 
 filename = input("Enter a filename: ")
+
 try:
-    with open(filename, "r") as file1:
-        answer_list = [line.split() for line in file1]
-    print(f"Successfully opened {filename}")
+    df_raw = pd.read_csv(filename, sep=" ", header=None)
 except:
     print("File cannot be found!")
 
-np_answer = np.array(answer_list)
+print("**** ANALYZING ****")
+
+df_exam = df_raw[0].str.split(',', n=1, expand=True)
+df_exam.rename(columns={0: "Student Code", 1: "Answers"}, inplace=True)
+
+
+def to_array(answer_str):
+    return np.array(answer_str.split(','))
+
+
+df_exam['Answers'] = df_exam['Answers'].apply(to_array)
+
+
+def check_code(code):
+    if not re.findall("^N[0-9]{8}", code[0:9]):
+        return code[0:9]
+
+
+def check_data(raw_str):
+    if len(raw_str.split(',')) != 26:
+        return raw_str
+
+
+def find_invalid_code(student_code):
+    if not re.findall("^N[0-9]{8}", student_code):
+        return False
+    else:
+        return True
+
+
+def find_invalid_answer(answer_arr):
+    if answer_arr.size != 25:
+        return False
+    else:
+        return True
+
+
+df_exam["Valid"] = df_exam["Student Code"].apply(find_invalid_code) & df_exam["Answers"].apply(find_invalid_answer)
+
+invalid_lines = df_exam["Valid"].values.sum()
+valid_lines = df_exam["Student Code"].count() - invalid_lines
+if invalid_lines == 0:
+    print("No errors found!")
+else:
+    print("Invalid line of data: does not contain exactly 26 values:")
+
+print("**** REPORT ****")
+
+print(df_exam)
+print(f"Total valid lines of data: {valid_lines}")
+print(f"Total invalid lines of data: {invalid_lines}")
